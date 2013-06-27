9a120060d2be9e71073223bacf8bb19e6066d470
Guille <guille@ggalmazor.com>
2013-06-27 12:19:55 +0200

    Código refactorizado para resolver el problema con manipulación de arrays estilo funcional

--- manipulador.js
+++ manipulador.js
@@ -1,8 +1,9 @@
 var pulses = {
-      ".": {signal: 'hi', duration: 1},
-      "-": {signal: 'hi', duration: 3},
-      " ": {signal: 'low', duration: 1},
-      "_": {signal: 'low', duration: 3}
+      ".": {id: 'dot', label: 'dit', signal: 'hi', duration: 1},
+      "-": {id: 'dash', label: 'dah', signal: 'hi', duration: 3},
+      "·": {id: 'inter-element-silence', label: ' ', signal: 'low', duration: 1},
+      " ": {id: 'inter-character-silence', label: '   ', signal: 'low', duration: 1},
+      "_": {id: 'inter-word-silence', label: '       ', signal: 'low', duration: 7}
     },
     codes = {
       a: '.-',
@@ -45,7 +46,7 @@ var pulses = {
       '.': '.-.-.-',
       ',': '--..--',
       '?': '..--..',
-      ' ': '_' // Hack para representar espacios entre palabras
+      ' ': ' '
     };
 
 function morsify(text) {
@@ -53,24 +54,38 @@ function morsify(text) {
       .toLowerCase()
       .split('')
       .map(function (char) {
-        return !!codes[char] ? codes[char] + ' ' : '';
+        return !!codes[char] ? codes[char] : '';
       });
 }
 
-function pulsify(code) {
-  return code
-      .trim()
-    // Juntar el espacio después de la última letra
-    // con el espacio antes de la siguiente palabra
-      .replace(/ _ /, '_')
+function pulsifySymbol(symbol) {
+  return symbol
       .split('')
-      .map(function (char) {
-        return pulses[char];
-      });
+      .reduce(function (elements, element) {
+        elements.push(pulses[element]);
+        elements.push(pulses['·']); // Añado un espacio entre elementos
+        return elements;
+      }, [])
+      .slice(0, -1); // Quito el espacio entre elementos final
+}
+function pulsify(morseCode) {
+  return morseCode
+      .reduce(function (pulsified, symbol) {
+        if (symbol != ' ') {
+          pulsified = pulsified.concat(pulsifySymbol(symbol));
+          pulsified.push(pulses[' ']); // Añado un espacio entre símbolos
+        } else {
+          pulsified = pulsified.slice(0, -1); // Quito el espacio entre símbolos precedente
+          pulsified.push(pulses['_']); // Añado un espacio entre palabras
+        }
+        return pulsified;
+      }, [])
+      .slice(0, -1); // Quito el espacio entre palabras final
 }
 
 var texto = '',
     morse = '',
+    morseCodes = [],
     stdin = process.stdin;
 
 stdin.setRawMode(true);
@@ -78,26 +93,26 @@ stdin.resume();
 stdin.setEncoding('utf8');
 process.stdin.on('data', function (key) {
   if (key == '\u0003')
-    terminar(texto, morse);
+    terminar(texto, morse, morseCodes);
   var morseKey = morsify(key);
-  texto += key;
-  morse += morseKey;
-  process.stdout.write(morseKey + ' ');
+  if (!!morseKey[0]) {
+    morseCodes = morseCodes.concat(morseKey);
+    texto += key;
+    morse += morseKey + '   ';
+    process.stdout.write(morseKey + '   ');
+  }
 });
 
-function terminar(texto, morsified) {
+function terminar(texto, morse, morseCodes) {
   process.stdout.write('\n\nEsto es lo que has escrito:\n');
   process.stdout.write(texto + '\n\n');
   process.stdout.write('\n\nEn morse sería esto:\n');
-  process.stdout.write(morsified + '\n\n');
+  process.stdout.write(morse + '\n\n');
   process.stdout.write('\n\nEn pulsos sería esto:\n');
-  var pulsified = pulsify(morsified);
-  pulsified.forEach(function (pulse) {
-    if (pulse.duration == 3)
-      process.stdout.write(pulse.signal.toUpperCase() + ' ');
-    else
-      process.stdout.write(pulse.signal + ' ');
+  pulsify(morseCodes).forEach(function (pulse) {
+    process.stdout.write(pulse.label);
   });
+
   process.stdout.write("\n");
   process.exit();
 }
