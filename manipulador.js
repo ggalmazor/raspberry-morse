212358533887a3afb0205e63692d225ac6f29d86
Guille <guille@ggalmazor.com>
2013-06-27 09:49:32 +0200

    Spike del traductor en tiempo real de texto a morse y su decodificación a pulsos con distintas duraciones

---
+++ manipulador.js
@@ -0,0 +1,100 @@
+var pulses = {
+      ".": {signal: 'hi', duration: 1},
+      "-": {signal: 'hi', duration: 3},
+      " ": {signal: 'low', duration: 1}
+    },
+    codes = {
+      a: '.-',
+      b: '-...',
+      c: '-.-.',
+      d: '-..',
+      e: '.',
+      f: '..-.',
+      g: '--.',
+      h: '....',
+      i: '..',
+      j: '.---',
+      k: '-.-',
+      l: '.-..',
+      m: '--',
+      n: '-.',
+      ñ: '-.',
+      o: '---',
+      p: '.--.',
+      q: '--.-',
+      r: '.-.',
+      s: '...',
+      t: '-',
+      u: '..-',
+      v: '...-',
+      w: '.--',
+      x: '-..-',
+      y: '-.--',
+      z: '--..',
+      0: '-----',
+      1: '.----',
+      2: '..---',
+      3: '...--',
+      4: '....-',
+      5: '.....',
+      6: '-....',
+      7: '--...',
+      8: '---..',
+      9: '----.',
+      '.': '.-.-.-',
+      ',': '--..--',
+      '?': '..--..',
+      ' ': '   '
+    };
+
+function morsify(text) {
+  return text
+      .toLowerCase()
+      .split('')
+      .map(function (char) {
+        return !!codes[char] ? codes[char] + ' ' : '';
+      });
+}
+
+function pulsify(code) {
+  return code
+      .split('')
+      .map(function (char) {
+        var pulse = pulses[char];
+        return  pulse;
+      });
+}
+
+var texto = '',
+    morse = '',
+    stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+process.stdin.on('data', function (key) {
+  if (key == '\u0003')
+    terminar(texto, morse);
+  var morseKey = morsify(key);
+  texto += key;
+  morse += morseKey;
+  process.stdout.write(morseKey + ' ');
+});
+
+function terminar(texto, morsified) {
+  process.stdout.write('\n\nEsto es lo que has escrito:\n');
+  process.stdout.write(texto + '\n\n');
+  process.stdout.write('\n\nEn morse sería esto:\n');
+  process.stdout.write(morsified + '\n\n');
+  process.stdout.write('\n\nEn pulsos sería esto:\n');
+  var pulsified = pulsify(morsified);
+  pulsified.forEach(function (pulse) {
+    if (pulse.duration == 3)
+      process.stdout.write(pulse.signal.toUpperCase() + ' ');
+    else
+      process.stdout.write(pulse.signal + ' ');
+  });
+  process.stdout.write("\n");
+  process.exit();
+}
+
